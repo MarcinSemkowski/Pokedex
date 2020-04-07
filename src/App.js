948d00dbf0878f5019d58adbbcdcbc5a10678c85
Marcin <marcin96semkowski@gmail.com>
2020-04-07 18:50:37 +0200

    NavBar Adding NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import NavBar from "./components/layout/NavBar";
 function App() {
   return (
-    <div className="App">
-        <h1>Marcin</h1>
+    <div className="App" style={{
+      backgroundColor: "#ef5350 !important"
+    }}>
+        <NavBar/>
     </div>
   );
 }
 
 export default App;
+ 
